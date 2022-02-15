098b25b75cc79c50e504ea6f89e4d22da11c7c50
Ece BAŞTÜRK <basturkece@gmail.com>
2022-02-15 05:14:21 +0300

    Update App.js
    Route Edit

--- apt_mngmnt_system/src/App.js
+++ apt_mngmnt_system/src/App.js
@@ -16,25 +16,19 @@ function App() {
     <BrowserRouter>
     
      <Switch>
-      <Route path='/' component={Login} exact/> 
-<Route path="*" component={NotFound} />
-
-
-
+    <Route path='/' component={Login} exact/> 
+    <Route path='/home' component={Home} />
+    <Route path='/user' component={User}/> */}
+    <Route path='/apartment' component={Apartment}/>
+     <Route path='/electricity' component={Electricity} />
+     <Route path='/water' component={Water} />
+     <Route path='/due' component={Due} />
+     <Route path='/resident' component={Resident} />    
+      <Route path="*" component={NotFound} />
+        
      </Switch>
     </BrowserRouter>
   );
 }
 
 export default App;
-//<Route path='/home' component={Home} />
-{/* <Route path='/resident' component={Resident} />
-<Route path="*" component={NotFound} />
-       <Route path='/user' component={User}/> */}
-      //  <Route path='/apartment' component={Apartment}/>
-
-      //  <Route path='/electricity' component={Electricity} />
-      //  <Route path='/water' component={Water} />
-      //  <Route path='/due' component={Due} />
-      // <Route path='/' component={Login} exact/> 
-      // <Route path='/resident' component={Resident} />
